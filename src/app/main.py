659438079ed844fd7eb501a822856fa6f02e1f17
RutamBhagat <rutambhagat@gmail.com>
2024-06-03 00:54:43 +0530

    Barebones Template

---
+++ src/app/main.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from src.app.models import models
+from src.app.db.database import engine
+from src.app.api import router as api_router
+from src.app.middleware import time_middleware, cors_middleware
+
+
+load_dotenv()
+
+app = FastAPI()
+
+# Connect to the database
+models.Base.metadata.create_all(bind=engine)
+
+# Middleware
+cors_middleware.middleware(app)
+app.middleware("http")(time_middleware.middleware)
+
+# Routes
+app.include_router(api_router)
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
